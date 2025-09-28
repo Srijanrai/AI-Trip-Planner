9ee2899599584ce5822526f4e04bc3827c408e44
Srijanrai <srijanrai61@gmail.com>
2025-09-28 12:05:58 +0530

    ai-integration

--- app/create-new-trip/_components/ChatBox.tsx
+++ app/create-new-trip/_components/ChatBox.tsx
@@ -1,27 +1,73 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { Send } from "lucide-react";
+import { Loader, Send } from "lucide-react";
+import axios from "axios";
+import EmptyBoxState from "./EmptyBoxState";
+type Message = {
+  role: string;
+  content: string;
+};
 function ChatBox() {
-  const onSend = () => {};
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [userInput, setUserInput] = useState<string>("");
+  const [loading, setLoading] = useState(false);
+  const onSend = async () => {
+    setUserInput("");
+    setLoading(true);
+    if (!userInput?.trim()) return;
+    const newMsg: Message = {
+      role: "user",
+      content: userInput,
+    };
+    setMessages((prev: Message[]) => [...prev, newMsg]);
+
+    const result = await axios.post("/api/aimodel", {
+      messages: [...messages, newMsg],
+    });
+
+    setMessages((prev: Message[]) => [
+      ...prev,
+      {
+        role: "assistant",
+        content: result?.data?.resp,
+      },
+    ]);
+    setLoading(false);
+  };
   return (
     <div className="h-[80vh] flex flex-col">
       <section className="flex-1 overflow-y-auto p-4">
-        <div className="flex justify-end mt-2">
-          <div className="max-w-lg bg-primary text-white px-4 py-2 rounded-lg">
-            User Msg
+        {messages?.length === 0 && <EmptyBoxState />}
+        {messages.map((msg: Message, index: number) => {
+          return msg.role == "user" ? (
+            <div className="flex justify-end mt-2" key={index}>
+              <div className="max-w-lg bg-primary text-white px-4 py-2 rounded-lg">
+                {msg.content}
+              </div>
+            </div>
+          ) : (
+            <div className="flex justify-start mt-2" key={index}>
+              <div className="max-w-lg bg-gray-100 text-black px-4 py-2 rounded-lg">
+                {msg.content}
+              </div>
+            </div>
+          );
+        })}
+        {loading && (
+          <div className="flex justify-start mt-2">
+            <div className="max-w-lg bg-gray-100 text-black px-4 py-2 rounded-lg">
+              <Loader className="animate-spin h-4 w-4" />
+            </div>
           </div>
-        </div>
-        <div className="flex justify-start mt-2">
-          <div className="max-w-lg bg-gray-100 text-black px-4 py-2 rounded-lg">
-            User Msg
-          </div>
-        </div>
+        )}
       </section>
       <section>
         <div className="border rounded-2xl p-4 relative">
           <Textarea
+            onChange={(e) => setUserInput(e.target.value)}
+            value={userInput}
             placeholder="Create a trip from Varanasi to Delhi"
             className="w-full h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none"
           />
